3de39bda5d344d6dc87fdb4e8d02d236e6f6ed9e
nunomeiras <108801198+nunomeiras@users.noreply.github.com>
2025-03-19 09:37:49 +0000

    Create default.ps1

---
+++ default.ps1
@@ -0,0 +1,11 @@
+choco install 7zip.install --yes
+choco install anydesk --yes
+echo aifrd5d | "C:\Program Files (x86)\AnyDesk\AnyDesk.exe" --set-password
+choco install firefox /l:pt-PT --yes
+choco install googlechrome --ignore-checksums --yes
+choco install notepadplusplus.install --yes
+choco install jre8 --yes
+choco install paint.net --yes
+choco install pdf24 --yes
+choco install vlc --yes
+choco install zoom --yes
