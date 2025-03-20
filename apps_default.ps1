4f8fb39529fd58ddbc22211e322d2733d7d11880
nunomeiras <108801198+nunomeiras@users.noreply.github.com>
2025-03-20 22:25:44 +0000

    Update and rename default.ps1 to apps_default.ps1

--- apps_default.ps1
+++ apps_default.ps1
@@ -1,6 +1,5 @@
 choco install 7zip.install --yes
 choco install anydesk --yes
-echo aifrd5d | "C:\Program Files (x86)\AnyDesk\AnyDesk.exe" --set-password
 choco install firefox /l:pt-PT --yes
 choco install googlechrome --ignore-checksums --yes
 choco install notepadplusplus.install --yes
