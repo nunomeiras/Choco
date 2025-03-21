15070f2a46407c674e64249aabfc193d4f81208a
nunomeiras <108801198+nunomeiras@users.noreply.github.com>
2025-03-21 10:07:26 +0000

    Update apps_basicas.ps1

--- apps_basicas.ps1
+++ apps_basicas.ps1
@@ -1,5 +1,5 @@
 choco install 7zip.install --yes
-choco install anydesk --yes
+choco install anydesk --yes --params="'/INSTALL /DESKTOPICON /UPDATETYPE:auto --set-password aifrd5d'
 choco install firefox --yes
 choco install googlechrome --ignore-checksums --yes
 choco install notepadplusplus.install --yes
@@ -9,4 +9,4 @@ choco install pdf24 --yes
 choco install vlc --yes
 choco install zoom --yes
 choco install --yes tightvnc --installArguments "ADDLOCAL=Server SERVER_REGISTER_AS_SERVICE=1 SERVER_ADD_FIREWALL_EXCEPTION=1 SET_USEVNCAUTHENTICATION=1 VALUE_OF_USEVNCAUTHENTICATION=1 SET_PASSWORD=1 VALUE_OF_PASSWORD=P@ssw0rd SET_USECONTROLAUTHENTICATION=1 VALUE_OF_USECONTROLAUTHENTICATION=1 SET_CONTROLPASSWORD=1 VALUE_OF_CONTROLPASSWORD=Penela01"
-
+choco install glpi-agent --yes --install-arguments="SERVER=https://glpi.cm-penela.pt TAG=InstalledByChocolatey RUNNOW=1 ADD_FIREWALL_EXCEPTION=1"
