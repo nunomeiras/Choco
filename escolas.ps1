535f2e33d622ec6f77e11e0fa6f1458edc777d2f
nunomeiras <108801198+nunomeiras@users.noreply.github.com>
2025-11-12 12:56:21 +0000

    Create escolas.ps1

---
+++ escolas.ps1
@@ -0,0 +1,11 @@
+choco install adobereader --yes
+choco install 7zip.install --yes
+choco install firefox --yes
+choco install googlechrome --ignore-checksums --yes
+choco install notepadplusplus.install --yes
+choco install jre8 --yes
+choco install paint.net --yes
+choco install pdf24 --yes
+choco install vlc --yes
+choco install zoom --yes
+choco install dotnet-8.0-desktopruntime --yes
