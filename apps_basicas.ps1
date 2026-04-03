9468620eee7088c2be289ceefdd54e57aeab090c
nunomeiras <108801198+nunomeiras@users.noreply.github.com>
2026-04-03 23:34:56 +0100

    Update apps_basicas.ps1

--- apps_basicas.ps1
+++ apps_basicas.ps1
@@ -1,12 +1,11 @@
+choco install adobereader -y
 choco install nanazip --yes
 choco install firefox --yes
 choco install googlechrome --ignore-checksums --yes
-choco install notepadplusplus.install --yes
+choco install notepadplusplus -y
 choco install jre8 --yes
-choco install paint.net --yes
 choco install pdf24 --yes
 choco install vlc --yes
 choco install zoom --yes
-choco install dotnet-8.0-desktopruntime --yes
-
+choco install vcredist-all -y
 choco install choco-upgrade-all-at-startup
